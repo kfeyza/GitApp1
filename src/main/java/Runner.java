01e8e61bbef71efe469151a05999f136caf776ec
feyza <k.feyza93@gmail.com>
2024-05-24 22:43:28 +0300

    v2

---
+++ src/main/java/Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+    public static void main(String[] args) {
+        //versiyon 1
+        System.out.println("Hello Git");
+        System.out.println("git add . staging areaya alir");
+
+        //versiyon2
+        System.out.println("git commit -m\" \" ile  version olusturulur");
+    }
+}
